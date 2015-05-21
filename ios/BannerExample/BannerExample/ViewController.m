a0f2d3d758c0efe07521cf13b376ca656cd915cc
chameleonliu <martinkro@aliyun.com>
2015-05-21 18:35:45 +0800

    ios demo

---
+++ ios/BannerExample/BannerExample/ViewController.m
@@ -0,0 +1,31 @@
+// Copyright (c) 2014 Google. All rights reserved.
+
+@import GoogleMobileAds;
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  // Replace this ad unit ID with your own ad unit ID.
+  self.bannerView.adUnitID = @"ca-app-pub-9880926895077532/8717636785";
+  self.bannerView.rootViewController = self;
+
+  GADRequest *request = [GADRequest request];
+  // Requests test ads on devices you specify. Your test device ID is printed to the console when
+  // an ad request is made. GADBannerView automatically returns test ads when running on a
+  // simulator.
+    //  request.testDevices = @[
+    //    @"2077ef9a63d2b398840261c8221a0c9a"  // Eric's iPod Touch
+    //  ];
+  [self.bannerView loadRequest:request];
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
